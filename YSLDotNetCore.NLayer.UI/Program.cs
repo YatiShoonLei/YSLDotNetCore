ee53ad5704361d6ef813a59b3803392fe47d08ba
YatiShoonLei <yati120000@gmail.com>
2024-07-03 14:19:48 +0630

    NLayer.UI

--- YSLDotNetCore.NLayer.UI/Program.cs
+++ YSLDotNetCore.NLayer.UI/Program.cs
@@ -3,5 +3,5 @@ using YSLDotNetCore.NLayer.BusinessLogic.Services;
 
 Console.WriteLine("Hello, World!");
 
-BL_Blog bl_Blog = new BL_Blog();
-bl_Blog.GetBlogs();
+//BL_Blog bl_Blog = new BL_Blog();
+//bl_Blog.GetBlogs();
